5574b8371832242257a93e23cb7b32505fe3722c
Fabricio Dorneles <fabriciodorneles@gmail.com>
2021-07-03 08:18:55 -0300

    Replace Temp & Inline Variable

--- src/statement.js
+++ src/statement.js
@@ -10,16 +10,19 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2,
     });
 
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
   for (const perf of invoice.performances) {
-    const play = plays[perf.playID];
-    const thisAmount = amountFor(perf, play);
+    const thisAmount = amountFor(perf, playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
+    if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
@@ -29,27 +32,25 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
   let result = 0;
-
   switch (play.type) {
     case 'tragedy':
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case 'comedy':
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
-
   return result;
 }
 
