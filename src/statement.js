d90599ab7959480b0518927fe28453d379c38d53
Fabricio Dorneles <fabriciodorneles@gmail.com>
2021-07-03 11:34:34 -0300

    enrich totals

--- src/statement.js
+++ src/statement.js
@@ -2,6 +2,8 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -44,19 +46,8 @@ function statement(invoice, plays) {
     if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5);
     return result;
   }
-}
 
-function renderPlainText(data) {
-  let result = `Statement for ${data.customer}\n`;
-  for (const perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
-  }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits`;
-
-  return result;
-
-  function totalAmount() {
+  function totalAmount(data) {
     let result = 0;
     for (const perf of data.performances) {
       result += perf.amount;
@@ -64,13 +55,24 @@ function renderPlainText(data) {
     return result;
   }
 
-  function totalVolumeCredits() {
+  function totalVolumeCredits(data) {
     let result = 0;
     for (const perf of data.performances) {
       result += perf.volumeCredits;
     }
     return result;
   }
+}
+
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`;
+  for (const perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+  }
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits`;
+
+  return result;
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US',
