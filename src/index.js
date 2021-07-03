839036361df2bc4059f25d7c6d78407777b24f56
Fabricio Dorneles <fabriciodorneles@gmail.com>
2021-07-03 05:56:14 -0300

    Crude Initial Function

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const Statement = require('./statement');
+
+const actualPlays = {
+  hamlet: {
+    name: 'Hamlet',
+    type: 'tragedy',
+  },
+  aslike: {
+    name: 'As You Like It',
+    type: 'comedy',
+  },
+  othello: {
+    name: 'Othello',
+    type: 'tragedy',
+  },
+};
+
+const actualInvoice = {
+  customer: 'BigCo',
+  performances: [
+    {
+      playID: 'hamlet',
+      audience: 55,
+    },
+    {
+      playID: 'aslike',
+      audience: 35,
+    },
+    {
+      playID: 'othello',
+      audience: 40,
+    },
+  ],
+};
+
+console.log(Statement(actualInvoice, actualPlays));
