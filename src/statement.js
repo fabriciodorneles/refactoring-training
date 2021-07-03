f564f8748d75b25e1a414ed450a6ee7b4126ea73
Fabricio Dorneles <fabriciodorneles@gmail.com>
2021-07-03 11:30:09 -0300

    enrich Volume Credits

--- src/statement.js
+++ src/statement.js
@@ -8,6 +8,7 @@ function statement(invoice, plays) {
     const result = { ...aPerformance };
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -36,6 +37,13 @@ function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result = Math.max(aPerformance.audience - 30, 0);
+    if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data) {
@@ -59,7 +67,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let result = 0;
     for (const perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -71,13 +79,6 @@ function renderPlainText(data) {
         currency: 'USD',
         minimumFractionDigits: 2,
       }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result = Math.max(aPerformance.audience - 30, 0);
-    if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
 
